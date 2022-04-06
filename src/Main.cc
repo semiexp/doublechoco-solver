987a1c82b1d0b70f5bafba92f03871b63c7ed886
semiexp <linthya.v@gmail.com>
2022-04-06 20:28:52 +0900

    Add URL parser

--- src/Main.cc
+++ src/Main.cc
@@ -1,28 +1,76 @@
 #include "Problem.h"
 #include "Solver.h"
 
-int main() {
-    // Random example to check if the solver is capable of finding a valid answer.
-    // Possible division:
-    // 112233
-    // 411223
-    // 444223
-    // 554422
-    // 544466
-    // 547777
-    const int height = 6;
-    const int width = 6;
-    int base[height][width] = {
-        {1, 1, 1, 1, 0, 0}, {1, 0, 0, 1, 1, 1}, {1, 1, 1, 0, 0, 1},
-        {1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 1, 1},
-    };
+#include <cstdlib>
+#include <string>
 
+int base36_to_int(char c) {
+    if ('0' <= c && c <= '9')
+        return c - '0';
+    else
+        return c - 'a' + 10;
+}
+
+Problem ParseURL(const std::string& url) {
+    std::string prefix("https://puzz.link/p?dbchoco/");
+    if (url.size() < prefix.size() || url.substr(0, prefix.size()) != prefix) {
+        abort();
+    }
+    std::string body = url.substr(prefix.size());
+    int height, width;
+    {
+        auto pos = body.find("/");
+        if (pos == std::string::npos)
+            abort();
+        width = std::stoi(body.substr(0, pos));
+        body = body.substr(pos + 1);
+    }
+    {
+        auto pos = body.find("/");
+        if (pos == std::string::npos)
+            abort();
+        height = std::stoi(body.substr(0, pos));
+        body = body.substr(pos + 1);
+    }
+    int p = 0, idx = 0;
     Problem problem(height, width);
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            problem.setColor(y, x, base[y][x]);
+    while (idx < height * width) {
+        int n = base36_to_int(body[p++]);
+        for (int i = 0; i < 5; ++i) {
+            if (idx >= height * width)
+                break;
+            problem.setColor(idx / width, idx % width, (n >> (4 - i)) & 1);
+            ++idx;
         }
     }
+    idx = 0;
+    while (idx < height * width) {
+        if ('g' <= body[p] && body[p] <= 'z') {
+            idx += base36_to_int(body[p]) - 15;
+            ++p;
+            continue;
+        }
+        int n;
+        if (body[p] == '-') {
+            n = (base36_to_int(body[p + 1]) << 4) | base36_to_int(body[p + 2]);
+            p += 3;
+        } else if (body[p] == '+') {
+            n = (base36_to_int(body[p + 1]) << 8) | (base36_to_int(body[p + 2]) << 4) | base36_to_int(body[p + 3]);
+            p += 4;
+        } else {
+            n = base36_to_int(body[p]);
+            ++p;
+        }
+        problem.setNum(idx / width, idx % width, n);
+        ++idx;
+    }
+    return problem;
+}
+
+int main(int argc, char** argv) {
+    Problem problem = ParseURL(argv[1]);
+    int height = problem.height();
+    int width = problem.width();
 
     std::optional<DoublechocoAnswer> ans = FindAnswer(problem);
     assert(ans.has_value());
