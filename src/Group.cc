db0241e0da626ff46727702033aebdb4d090b1ab
semiexp <linthya.v@gmail.com>
2023-07-17 11:30:07 +0900

    Allow cells which belong to no group in GroupInfo

--- src/Group.cc
+++ src/Group.cc
@@ -11,7 +11,10 @@ GroupInfo::GroupInfo(Grid<int>&& group_id) : group_id_(group_id) {
     groups_offset_ = std::vector<int>(max_group_id + 2, 0);
     for (int y = 0; y < group_id_.height(); ++y) {
         for (int x = 0; x < group_id_.width(); ++x) {
-            ++groups_offset_[group_id_.at(y, x) + 1];
+            int id = group_id_.at(y, x);
+            if (id >= 0) {
+                ++groups_offset_[id + 1];
+            }
         }
     }
     for (int i = 1; i < groups_offset_.size(); ++i) {
@@ -21,7 +24,10 @@ GroupInfo::GroupInfo(Grid<int>&& group_id) : group_id_(group_id) {
     groups_raw_ = std::vector<std::pair<int, int>>(group_id_.height() * group_id_.width());
     for (int y = 0; y < group_id_.height(); ++y) {
         for (int x = 0; x < group_id_.width(); ++x) {
-            groups_raw_[next_pos[group_id_.at(y, x)]++] = std::make_pair(y, x);
+            int id = group_id_.at(y, x);
+            if (id >= 0) {
+                groups_raw_[next_pos[id]++] = std::make_pair(y, x);
+            }
         }
     }
 }
